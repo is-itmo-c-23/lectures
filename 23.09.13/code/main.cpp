3184bd6c92830ed6126f47159fe23bbd1043f79d
Alexander Khvastunov <spbdeveloper@yandex.ru>
2023-09-14 00:24:32 +0300

    add lecture1

---
+++ 23.09.13/code/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+
+// 1й полупоток
+int main(int, char**){
+
+    char ch = 22    ;
+    std::cout << ch << "\n";
+    std::cout << "Hello, \'World\'!\n";
+
+
+    int day = 0;  // Monday
+
+    // if(day == 6) {
+    //     sleep()
+
+    //     day = 10;
+    // }
+
+    enum Color {
+        RED,
+        GREEN,
+        BLUE
+    };
+
+
+    Color c = RED;
+
+    //c = c + 1;
+
+   // std::cout << 1 + 2.0 << std::endl;
+
+    std::cout << (int)2.7 << std::endl;
+
+
+}
+
+
+// 2й полупоток
+
+// /*
+// 10 = 1010
+//   1 = 000...000001
+//  10 = 000...001010
+// -10 = 111...110101 // обратный
+// -10 = 111...110110 // дополнительный
+//  -1 = 111...111110
+
+
+
+
+// -10 = 100...001010 // прямой
+
+
+//   0 =
+
+// float
+
+// _ ___m______ ______k________
+
+// sign * 0.m * 10^k
+
+// */
+// int main(int, char**) {
+//    char c = 'a';
+
+//    //std::cout << (int)2.45564654 << std::endl;
+
+//     int n = -2, a = 1, b = 2, z = 20;
+//     if (n > 0) {
+//         if(a > b) {
+//             z = a;
+//         }
+//     }
+//     else {
+//         z = b;
+//     }
+
+
+
+
+//     std::cout << z << std::endl;
+
+// //    std::cout << "Hello world!\n";
+
+
+// //    int day = 0; // 0 -  Monday 6 -
+
+
+
+// //    if(day == 6) {
+// //         sleep();
+// //         day = 10;
+// //    }
+
+// }
+
+
